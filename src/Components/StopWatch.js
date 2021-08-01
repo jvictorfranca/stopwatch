45ece776b7c30249b66f6d037184f126d6e655ea
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-08-01 03:19:29 -0300

    coloca input de tempo em minutos

--- src/Components/StopWatch.js
+++ src/Components/StopWatch.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import Watch from './Watch'
+import UnitsInput from './UnitsInput'
 
 class StopWatch extends React.Component {
   constructor(props){
     super(props)
       this.state = {
-        units: 864000,
+        units: 0,
+        startingUnits: 864000,
       }  
       this.startCount = this.startCount.bind(this)
       this.stopCount = this.stopCount.bind(this)
-    
+      this.resetUnits = this.resetUnits.bind(this)
+      this.setStartUnits = this.setStartUnits.bind(this)
   }
-  componentDidUpdate(props){
+  componentDidUpdate(){
     if (this.state.units === 0) {
       this.stopCount()
     }
@@ -28,13 +31,28 @@ stopCount () {
   clearInterval(this.countConstant)
   this.countConstant = undefined
 }
+resetUnits () {
+  this.setState(()=>({
+    units: this.state.startingUnits
+  }))
+  this.stopCount()
+}
+
+async setStartUnits (number) {
+   this.setState(()=>({
+    startingUnits: number
+  }), ()=>{
+  this.resetUnits()
+  })}
 
   render () {
     const {units} = this.state
    return (
    <div>
+     <UnitsInput updateValues={this.setStartUnits}></UnitsInput>
      <button onClick={this.startCount}>Start</button>
      <button onClick={this.stopCount}>Stop</button>
+     <button onClick={this.resetUnits}>Reset</button>
      <Watch  units= {units}></Watch>
    </div>
    )
